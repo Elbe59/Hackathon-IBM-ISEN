69ee4e0c9b97bb611db49d9b68d4f88572ff49e6
AlioptK <Hupode)6>
2021-03-27 11:41:12 +0100

    Fixing bug

--- App.js
+++ App.js
@@ -3,134 +3,140 @@ import {
   SafeAreaView, View, Button, RefreshControl, StyleSheet, Text,TextInput, ScrollView
 } from 'react-native';
 import MessageBubble from './Components/MessageBubble';
-import listMessages from './messagesInitiauxBot.json'
+import messagesInitiauxBot from './messagesInitiauxBot.json'
 
 
 const App = () => {
-      const [refreshing, setRefreshing] = useState(false);
-      const [inputText, setInputText] = useState("");
-      const [sessionId, setSessionId] = useState("");
+  const [refreshing, setRefreshing] = useState(false);
+  const [inputText, setInputText] = useState("");
+  const [sessionId, setSessionId] = useState("");
+  const [dataSource, setDataSource] = useState([]);
+  const scrollViewRef = useRef();
 
-      var msg_from_bot_1 = {"mine":false,"text":"Bonjour à toi ! Moi c'est Botty !\nJe vais te proposer un jeu:\n\nVoici 3 indices, à toi d'essayer de résoudre mon énigme pour rentrer dans le classement:","horaire":getCurrentDate(),"isSessionOff":false};
-      var msg_from_bot_2 = {"mine":false,"text":"Indice 1:\nX = Trouver le nombre de 6 chiffres se positionnant après la première occurence de 036695 dans les décimales de PI. Convertir ce nombre de la base10 en base26\n\nIndice 2:\nY = https://pasteboard.co/074 065 051 049 084 077 048 046 112 110 103/\n\nIndice 3:\nZ = 'X'+'Y' Lille","horaire":getCurrentDate(),"isSessionOff":false};
+  const url = "https://nodejs-express-app-cxlkb-2020-11-30.eu-gb.mybluemix.net/ai"
 
-      const url = "https://nodejs-express-app-cxlkb-2020-11-30.eu-gb.mybluemix.net/ai"
-      const wait = (timeout) => {
-  return new Promise(resolve => setTimeout(resolve, timeout));
-}
+  const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+  }
+  
+  useEffect(() => {
+    getFirstMessages();
+    getSession(url);
+  }, []) 
+  
+  const getFirstMessages = () => {
+    let data = messagesInitiauxBot;
+    for(let i = 0; i < data.length; i++)
+      data[i].horaire = getCurrentDate();
+    setDataSource(data);
+  }
 
-    useEffect(() => {
-      getSession(url);
-    }, []) 
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    wait(100).then(() => setRefreshing(false)); //Don't need to wait to load the messages
+  }, []);
 
-    const onRefresh = useCallback(() => {
-        setRefreshing(true);
-        wait(100).then(() => setRefreshing(false)); //Don't need to wait to load the messages
-      }, []);
-    const refreshAndAddMessage = (mine,textMessage) =>{
-      if(textMessage!=""){
-        if(mine){  // Si j'envoie un message, je l'envoie aussi au Bot
-          sendMessageToBot(textMessage);
-        }
-        dataSource.push({"mine":mine,"text":textMessage,"horaire":getCurrentDate()});
-        onRefresh();
-
-        submitText();
+  const refreshAndAddMessage = (mine,textMessage) =>{
+    if(textMessage!=""){
+      if(mine){  // Si j'envoie un message, je l'envoie aussi au Bot
+        sendMessageToBot(textMessage);
       }
+      let dataSource_temp = dataSource;
+      dataSource_temp.push({"mine":mine,"text":textMessage,"horaire":getCurrentDate()});
+      setDataSource(dataSource_temp);
+      onRefresh();  
+      submitText();
     }
-    const submitText = () => {
-      setInputText("")
-    }
-    const getSession = () =>{
-      fetch(url + '/session').then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Something went wrong');
+  } 
+
+  const submitText = () => {
+    setInputText("")
+  }
+
+  const getSession = () =>{
+    fetch(url + '/session').then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong');
+      }
+    })
+    .then((responseJson) => {
+      setSessionId(responseJson.response);
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  }    
+
+  const sendMessageToBot = (messageText) =>{
+    fetch(url, { 
+        method: "POST", 
+        headers: { 'Content-Type': 'application/json' }, 
+        body: JSON.stringify({sessionId: sessionId, reqText: messageText})
+    })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong');
+      }
+    })
+    .then((responseJson) => {
+      refreshAndAddMessage(false,responseJson.response);
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  }
+
+  return(
+    <SafeAreaView style={{flex: 1, flexDirection: 'column',paddingTop: 20,paddingBottom:10}}>        
+      <ScrollView
+        ref={scrollViewRef}
+        onContentSizeChange={() => scrollViewRef.current.scrollToEnd({ animated: true })}
+        style={{flex: 2}} 
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>} 
+        showsVerticalScrollIndicator={false}
+      >
+        { dataSource.length > 0 &&
+          dataSource.map((message, index) => {
+            
+            console.log(message);
+
+            return (
+              <MessageBubble
+                key = {index}
+                mine = {message.mine}
+                text = {message.text}
+                horaire = {message.horaire}
+              />
+            )
+
+          })
         }
-      })
-      .then((responseJson) => {
-          //console.log(responseJson);
-          setSessionId(responseJson.response);
-      })
-      .catch((error) => {
-        console.log(error)
-      });
-    }    
-
-    const sendMessageToBot = (messageText) =>{
-      //console.log(sessionId);
-      fetch(url, { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({
-        sessionId: sessionId, reqText: messageText
-      })
-    }).then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Something went wrong');
-        }
-      })
-      .then((responseJson) => {
-          //console.log(responseJson);
-          //console.log(responseJson.response);
-          refreshAndAddMessage(false,responseJson.response);
-      })
-      .catch((error) => {
-        console.log(error)
-      });
-    }  
-
-    var dataSource = listMessages;
-
-    const scrollViewRef = useRef();
-
-    return(
-        <SafeAreaView style={{flex: 1, flexDirection: 'column',paddingTop: 20,paddingBottom:10}}>        
-            <ScrollView
-              ref={scrollViewRef}
-              onContentSizeChange={() => scrollViewRef.current.scrollToEnd({ animated: true })}
-              style={{flex: 2}} 
-              refreshControl={
-                <RefreshControl
-                  refreshing={refreshing}
-                  onRefresh={onRefresh}/>
-              } 
-              showsVerticalScrollIndicator={false}
-            >
-            {
-              dataSource.map(addMessageBubble)
-            }
-          </ScrollView>
-          <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
-          <TextInput
-          style={styles.input}
-          placeholder='Ajouter votre texte ...'
-          onChangeText={(input) => setInputText(input)}
-          value={inputText}
-          onKeyPress={ (event) => {
-            if(event.nativeEvent.key == "Enter"){
-              refreshAndAddMessage(true, inputText)
-            } 
+      </ScrollView>
+      <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
+        <TextInput
+        style={styles.input}
+        placeholder='Ajouter votre texte ...'
+        onChangeText={(input) => setInputText(input)}
+        value={inputText}
+        onKeyPress={ (event) => {
+          if(event.nativeEvent.key == "Enter"){
+            refreshAndAddMessage(true, inputText)
+          } 
         }}
-          />
+        />
         <View style={{marginRight: 20, marginTop: 20, alignSelf: 'center'}}>
           <Button
-          title="Envoyer"
-          onPress={() => refreshAndAddMessage(true, inputText)}/>
+            title="Envoyer"
+            onPress={() => refreshAndAddMessage(true, inputText)}
+          />
         </View>
       </View>
-      </SafeAreaView>
-    )
-  }
-const addMessageBubble = (message,key) =>{
-    return(
-    <MessageBubble
-        key = {key}
-        mine = {message.mine}
-        text = {message.text}
-        horaire = {message.horaire} //getCurrentDate()}
-    />
-    );
+    </SafeAreaView>
+  );
 }
 
 const getCurrentDate = () => {
@@ -142,25 +148,21 @@ const getCurrentDate = () => {
   if (dateHours.toLocaleString().length < 2){
     dateHours = '0'+dateHours;
   }
-
   let date = dateHours + ':' + dateMin;
   return date;
 };
 
-
-
-
 const styles = StyleSheet.create({
   input:{
-  marginTop: 20,
-  marginLeft: 20,
-  marginRight: 20,
-  padding: 10,
-  borderWidth: 0.5,
-  borderRadius: 4,
-  backgroundColor: "#fff",
-  flex: 4,
-  flexDirection: 'row'
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    padding: 10,
+    borderWidth: 0.5,
+    borderRadius: 4,
+    backgroundColor: "#fff",
+    flex: 4,
+    flexDirection: 'row'
   }
 });
 
---
+++ ClueY.js
@@ -0,0 +1,117 @@
+import { View, StyleSheet, ScrollView, Text, Dimensions } from "react-native";
+import React, {useState, useEffect} from 'react';
+import * as Animatable from 'react-native-animatable';
+
+export const ClueY = props => {     
+    // Initializing variables
+    let scrollViewRef = React.createRef();
+    let [hide_ScrollView_X, setHide_ScrollView_X] = useState(false);
+    let [hide_XRaw, setHide_XRaw] = useState(false);
+
+    // Props inputs
+    let Pi = props.Pi;
+    let XRaw = props.XRaw;
+    let X = props.X;
+    let showDecimalPi = props.showDecimalPi;
+    let decimalPi = props.decimalPi;
+    
+    // Component did mount
+    useEffect(() => {
+        setTimeout(() => {
+            try {
+                scrollViewRef.scrollToEnd({animated: true, duration:3000});
+            } catch (error) {
+                console.error("Error: Couldn't scroll to the end.");
+            }
+        }, 2200);
+    },[]);
+    
+    return(
+        <View style={{flex:1}}>       
+            { !hide_ScrollView_X &&
+                <Animatable.View 
+                    onAnimationEnd={() => setHide_ScrollView_X(true)}
+                    animation="fadeOutLeft"
+                    delay={7000}
+                    duration={700}
+                    direction="normal"
+                >
+                    <ScrollView
+                        ref={(r) => scrollViewRef=r}
+                        showsVerticalScrollIndicator={false}
+                        scrollEnabled={false}
+                    >
+                        <Text style={styles.PI_Wrapper}>
+                            {Pi}
+                            <Text style={styles.PI_Number_Found}>
+                                {XRaw}
+                            </Text>
+                        </Text>
+                        { showDecimalPi &&
+                            <View style={styles.Animation_Wrapper_Bottom}>
+                                <Text style={styles.PI_Decimal_Index}>
+                                    {decimalPi}th decimal of π
+                                </Text>
+                                { !hide_XRaw &&
+                                    <Animatable.Text 
+                                        onAnimationEnd={() => setHide_XRaw(true)}
+                                        animation="fadeOutLeft"
+                                        delay={2000}
+                                        duration={800}
+                                        direction="normal"
+                                        style={styles.Number_Raw_Animation}
+                                    >
+                                        {XRaw}
+                                    </Animatable.Text>
+                                }
+                                <Animatable.Text  
+                                    animation="fadeInRight"
+                                    delay={2800}
+                                    direction="normal"
+                                    style={styles.Number_Converted_Animation}
+                                >
+                                    {X}
+                                </Animatable.Text>
+                            </View>
+                        }
+                    </ScrollView>
+                </Animatable.View>
+            }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    PI_Wrapper : {
+        textAlign:"justify",
+        fontSize:36,
+        fontWeight:"600",
+        paddingTop:100
+    },
+    PI_Number_Found : {
+        fontSize:36,
+        fontWeight:"bold",
+        color:"red"
+    },
+    Animation_Wrapper_Bottom : {
+        height:400,
+        width:'100%',
+        alignItems:"center",
+        paddingBottom:Dimensions.get('window').height/2.5
+    },
+    PI_Decimal_Index : {
+        textAlign:"center",
+        fontSize:22,
+        marginVertical: 50
+    },
+    Number_Raw_Animation : {
+        color:"red",
+        fontSize:70,
+        fontWeight:"700"
+    },
+    Number_Converted_Animation : {
+        color:"red",
+        fontSize:70,
+        fontWeight:"700"
+    }
+});
